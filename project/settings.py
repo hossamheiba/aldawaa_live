7a695c82a091c107f106e1c584f770e8f22fcb1f
hossam <hossamheiba7@gmail.com>
2024-12-10 03:24:10 +0300

    update

--- project/settings.py
+++ project/settings.py
@@ -148,8 +148,7 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+MEDIA_ROOT = os.path.join('/mnt/data', 'media')  
 
 # Default primary key field type
 
